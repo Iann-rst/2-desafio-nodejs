7406c17a1cfa65ff983bb2842d564247916ef691
Iann-rst <iannrst11@gmail.com>
2022-11-27 20:09:37 -0300

    feat: Verificar se existe um todo

--- src/index.js
+++ src/index.js
@@ -39,7 +39,33 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      message: 'User not found'
+    });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      message: 'Invalid ID'
+    });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      message: 'Todo not found'
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
